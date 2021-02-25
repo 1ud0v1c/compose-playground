b0b5b469d744dbce045daa24db20579f4fea6054
Ludovic Vimont <ludovic.vimont@gmail.com>
2021-02-25 15:33:40 +0100

    Re-add Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center">Jetpack Compose - Playground</h1>
+
+This repository only goal is to play with the new library [Jetpack Compose](https://developer.android.com/jetpack/compose).
+
+## Links
+
+- [To begin](https://developer.android.com/jetpack/compose/documentation)
+- [Tutorial](https://developer.android.com/jetpack/compose/tutorial)
+- [Jetpack compose show](https://www.youtube.com/watch?v=vRjJAWh6JPE&list=PLWz5rJ2EKKc98J4VE1glWGt5b90VfXZ1e&index=1)
+
--- app/src/main/java/com/ludovic/vimont/composeplayground/MainActivity.kt
+++ app/src/main/java/com/ludovic/vimont/composeplayground/MainActivity.kt
@@ -3,11 +3,15 @@ package com.ludovic.vimont.composeplayground
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ludovic.vimont.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : AppCompatActivity() {
@@ -25,7 +29,12 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text = "Hello $name!")
+        Text(text = "42")
+    }
 }
 
 @Preview(showBackground = true)
